ccc756cc273dbf808041b500be755ac05e25c37c
Victor Kazuyuki Shinya <contatovictorshinya@gmail.com>
2017-05-06 21:11:46 -0300

    Database credentials

--- src/br/com/ebookapp/database/RequestConnection.java
+++ src/br/com/ebookapp/database/RequestConnection.java
@@ -6,8 +6,8 @@ import java.sql.DriverManager;
 public class RequestConnection {
 	private static Connection connection;
 	private static String url = "jdbc:postgresql://ebookapp.postgresql.dbaas.com.br:5432/ebookapp";
-	private static String username = "";
-	private static String password = "";
+	private static String username = "ebookapp";
+	private static String password = "rootebook123";
 	
 	public static synchronized Connection getConnection() {
 		if (connection == null) {
